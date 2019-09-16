0db6c12e517603cd81533665fe05d16a520f6da3
cougarharlowe@gmail.com <fowlfighter7297>
2019-09-16 13:39:30 -0700

    almost finished the chronometer

--- app/src/main/java/com/mistershorr/stopwatch/MainActivity.java
+++ app/src/main/java/com/mistershorr/stopwatch/MainActivity.java
@@ -3,9 +3,9 @@ package com.mistershorr.stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.os.SystemClock;
 import android.util.Log;
+import android.view.KeyboardShortcutGroup;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
@@ -15,8 +15,13 @@ public class MainActivity extends AppCompatActivity {
     private Chronometer chronometerTime;
     private Button buttonStartstop;
     private Button buttonReset;
-    private long Base;
+    private long pausedTime = 0;
+    private long base = 0;
     private boolean isRunning = false;
+    public static final String KEY_SAVED_BASE = "chronometer base";
+    public static final String KEY_SAVED_PAUSEDTIME = "saved passed time";
+    public static final String KEY_SAVED_CURRENTTIME = "display time";
+    public static final String KEY_SAVED_ISRUNNING = "boolean";
     public static final String TAG = MainActivity.class.getSimpleName();
 
     // Look up the Log class for Android.
@@ -43,8 +48,15 @@ public class MainActivity extends AppCompatActivity {
         wireWidgets();
         setListeners();
         //chronometerTime();
+        if( savedInstanceState != null) {
+            chronometerTime.setBase(savedInstanceState.getLong(KEY_SAVED_BASE));
+            isRunning = savedInstanceState.getBoolean(KEY_SAVED_ISRUNNING);
+            pausedTime = savedInstanceState.getLong(KEY_SAVED_PAUSEDTIME);
+            //chronometerTime.start();
+        }
 
     }
+
 
     /*private void chronometerTime() {
         new Chronometer()
@@ -68,20 +80,39 @@ public class MainActivity extends AppCompatActivity {
                     // base should be old base + any paused time
                     // paused time is the diff between when you stopped it and now
                     // current time SystemClock.elapsedRealtime()
-                    chronometerTime.setBase(Base);
+                    pausedTime = pausedTime - SystemClock.elapsedRealtime();
+                    if(base != 0) {
+                        base = base - pausedTime;
+                        chronometerTime.setBase(base);
+                    } else {
+                        chronometerTime.setBase(SystemClock.elapsedRealtime()); }
                     chronometerTime.start();
-                    isRunning = false;
+                    isRunning = true;
+                    buttonStartstop.setText("Stop");
 
                 } else {
 
                     chronometerTime.stop();
-                    Base = chronometerTime.getBase();
-                    isRunning = true;
+                    base = chronometerTime.getBase();
+                    pausedTime = SystemClock.elapsedRealtime();
+                    isRunning = false;
+                    buttonStartstop.setText("Start");
 
                 }
 
 
 
+            }
+        });
+
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometerTime.setBase(SystemClock.elapsedRealtime());
+                base = 0;
+                isRunning = false;
+                chronometerTime.stop();
+                buttonStartstop.setText("Start");
             }
         });
     }
@@ -126,4 +157,14 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.d(TAG, "onDestroy: ");
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(KEY_SAVED_BASE, chronometerTime.getBase());
+        outState.putLong(KEY_SAVED_PAUSEDTIME, pausedTime);
+        outState.putLong(KEY_SAVED_CURRENTTIME, SystemClock.elapsedRealtime());
+        outState.putBoolean(KEY_SAVED_ISRUNNING, isRunning);
+    }
 }
+
